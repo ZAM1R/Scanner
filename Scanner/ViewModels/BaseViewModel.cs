1d639df0ff409b9e0ca164cca4dd6870c6c26531
Milad Dehghan <milad.dehghan@hotmail.com>
2022-10-11 11:00:26 +0330

    Update view model

--- Scanner/ViewModels/BaseViewModel.cs
+++ Scanner/ViewModels/BaseViewModel.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace Scanner.ViewModel
+namespace Scanner.ViewModels
 {
     public class BaseViewModel
     {
---
+++ Scanner/ViewModels/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Prism.Commands;
+using Scanner.Interfaces;
+using Scanner.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scanner.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+
+        public DelegateCommand StartScanCommand { get; private set; }
+        private readonly IScanService _scanService;
+
+        public MainViewModel(IScanService scanService)
+        {
+
+            StartScanCommand = new DelegateCommand(StartScan);
+
+            _scanService = scanService;
+            ScannedIpList = new() { };
+          
+        }
+
+        public async void StartScan()
+        {
+            ScannedIpList = await _scanService.Scan(ScannedIpList, _ip, _startPort, _endPort);
+        }
+
+        public string _ip;
+        public string Ip { get => _ip; set { SetProperty(ref _ip, value); } }
+
+
+        public int _startPort;
+        public int StartPort { get => _startPort; set { SetProperty(ref _startPort, value); } }
+
+
+        public int _endPort;
+        public int EndPort { get => _endPort; set { SetProperty(ref _endPort, value); } }
+
+
+        public ObservableCollection<Scan> _scannedIpList;
+        public ObservableCollection<Scan> ScannedIpList { get => _scannedIpList; set { SetProperty(ref _scannedIpList, value); } }
+
+    }
+}
